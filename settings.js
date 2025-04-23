7d3692e2c035420a3d292980c1b978e68b3b1eae
ashiqshaikh115 <ifa07661@gmail.com>
2025-04-23 08:11:15 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Saqib Imran",
+  botOwner: 'Saqib Imran', // Saqib Imran
+  ownerNumber: '923285429630', //923285429630
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
